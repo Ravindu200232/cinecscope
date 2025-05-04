d9a7f3e05f06f935fb2b69e73209448b8fa49bdc
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2025-05-04 14:45:51 +0530

    Session 07

---
+++ mflix-dashboard/app/page.jsx
@@ -0,0 +1,16 @@
+import HeaderNav from "../components/header-nav";
+
+
+
+
+
+//SSR-Server Side REndered - Server Component
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen ">
+      <HeaderNav/>
+      <main className="flex-1  bg-primary min-h-screen">Main Section</main>
+      <footer className=" bg-amber-400 h-72 ">Footer Section</footer>
+    </div>
+  );
+}
