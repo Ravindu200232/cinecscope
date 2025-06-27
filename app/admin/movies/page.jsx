2bc4a448008e201b667deb055194551062eaac01
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2025-06-27 14:50:47 +0530

    update

--- app/admin/movies/page.jsx
+++ app/admin/movies/page.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import AddMovieDialog from './add-movie-dialog'
-import MovieData from './movie-data'
+import AddMovieDialog from "./add-movie-dialog";
+import MovieData from "./movie-data";
+import MovieSelectors from "./movie-selectors";
 
-export default function MoviePage() {
+// Server Component
+export default async function MoviesPage(props) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+
+  // space-y-4: 16px
   return (
-    <div className='space-y-4'>
-        <div className='flex items-center justify-between'>
-            <div>
-                <h2 className='text-3xl font-bold tracking-tighter'>Movies</h2>
-                <p className='text-muted-foreground tracking-wider'>Manage your movie catalog</p>
-            </div>
-
-
-            <AddMovieDialog/>
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <div>
+          <h2 className="text-3xl font-bold tracking-tight">Movies</h2>
+          <p className="text-muted-foreground">Manage your movie catalog</p>
         </div>
 
-        <MovieData/>
+        <AddMovieDialog />
+      </div>
+
+      <MovieSelectors />
+
+      <MovieData query={query} />
     </div>
-  )
+  );
 }
