f23a9f35f57811ad2300dcfed2f2d3544c57677b
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2025-06-27 10:50:12 +0530

    session 16

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,29 +1,26 @@
-import React from 'react'
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar"
-import AdminSidebar from '@/components/ui/admin-sidebar'
 
+import AdminSidebar from "@/components/ui/admin-sidebar";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import UserNav from "@/components/user-nav";
 
-export default function AdminLayout({children}) {
+export default function AdminLayout({ children }) {
   return (
-    <div><SidebarProvider>
-      {/* 1 Side bar section */}
-        <AdminSidebar/>
-  
+    <SidebarProvider>
+      {/* 1. Sidebar Section */}
+      <AdminSidebar />
 
-{/* 2 Main content Section */}
-        <SidebarInset>
-       <header className='sticky top-0 z-50 border-b bg-background'>
-        <div className='h-16 flex items-center justify-between px-4'>
-          <h1 className='text-xl font-bold'>Admin Dashboard</h1>
+      {/* 2. Main Content Section */}
+      <SidebarInset>
+        <header className="sticky top-0 z-50 border-b bg-background">
+          <div className="flex h-16 items-center justify-between px-4">
+            <h1 className="text-xl font-bold">Admin Dashboard</h1>
 
-          {/*3 user Dropdown Navigation */}
-          <div className='bg-red-600 rounded-full h-10 w-10 flex justify-center items-center'>Hs</div>
-        </div>
-       </header>
-
-       <div className='flex-1 p-4 md:p-8'>{children}</div>
-        </SidebarInset>
-        
-        </SidebarProvider></div>
-  )
+            {/* 3. User Dropdown Navigation */}
+            <UserNav />
+          </div>
+        </header>
+        <div className="flex-1 p-4 md:p-8">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
